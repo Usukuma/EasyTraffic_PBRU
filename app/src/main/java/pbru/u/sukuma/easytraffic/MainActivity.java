fb13cfd97f81204e18cccb68c70614e69bd91e61
unknown <sukuma_ay@yahoo.com>
2015-08-18 13:44:59 +0700

    Before Break2

--- app/src/main/java/pbru/u/sukuma/easytraffic/MainActivity.java
+++ app/src/main/java/pbru/u/sukuma/easytraffic/MainActivity.java
@@ -27,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
     }//main method
 
     private void createListView() {
-        String[] strTitle = new String[20];
-        StrTitle[0] = "ห้ามเลี้ยวซ้าย;
+        String[] StrTitle = new String[20];
+        StrTitle[0] = "ห้ามเลี้ยวซ้าย";
         StrTitle[1] = "ห้ามเลี้ยวขวา";
         StrTitle[2] = "ตรงไป";
         StrTitle[3] = "เลี้ยวซ้าย";
@@ -48,6 +48,19 @@ public class MainActivity extends AppCompatActivity {
         StrTitle[17] = "จำกัดความเร็ว";
         StrTitle[18] = "จำกัดความกว้าง";
         StrTitle[19] = "จำกัดความสูง";
+
+
+        int[] intImage = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
+                R.drawable.traffic_04, R.drawable.traffic_05, R.drawable.traffic_06,
+                R.drawable.traffic_07, R.drawable.traffic_08, R.drawable.traffic_09,
+                R.drawable.traffic_10, R.drawable.traffic_11, R.drawable.traffic_12,
+                R.drawable.traffic_13, R.drawable.traffic_14, R.drawable.traffic_15,
+                R.drawable.traffic_16, R.drawable.traffic_17, R.drawable.traffic_18,
+                R.drawable.traffic_19, R.drawable.traffic_20};
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intImage, StrTitle);
+        trafficListView.setAdapter(objMyAdapter);
+
     }
 
     private void initialWidget() {
